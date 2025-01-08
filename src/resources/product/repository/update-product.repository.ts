3c69b978f63ab2a4c3ff2862ecba6bd582f0b6fb
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-08 16:29:35 -0300

    feat: add update product repository

---
+++ src/resources/product/repository/update-product.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateProductRepositoryContract } from 'src/resources/product/contract/update-product.contract';
+import { Product } from 'src/resources/product/entity/product.entity';
+import { ProductModel } from 'src/resources/product/model/product.model';
+import { DataSource } from 'typeorm';
+
+@Injectable()
+export class UpdateProductRepository
+  implements UpdateProductRepositoryContract
+{
+  constructor(private readonly ds: DataSource) {}
+
+  async updateProduct(input: {
+    id: number;
+    name?: string;
+    image?: string;
+    description?: string;
+    price?: number;
+    categoryId?: number;
+    subcategoryId?: number;
+    discountPrice?: number;
+    active: boolean;
+  }): Promise<Product> {
+    const result = await this.ds
+      .createQueryBuilder()
+      .update(ProductModel)
+      .set({
+        id: input.id,
+        name: input.name,
+        image: input.image,
+        description: input.description,
+        price: input.price,
+        categoryId: input.categoryId,
+        subcategoryId: input.subcategoryId,
+        discountPrice: input.discountPrice,
+        active: input.active,
+      })
+      .where('id = :id', { id: input.id })
+      .returning('*')
+      .execute();
+
+    const product = result.raw[0];
+    return product;
+  }
+}
