bf72fe140652037732e6c8a3fb289712131d46c3
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-08 14:15:54 -0300

    feat: add update product service

---
+++ src/resources/product/service/update-product.service.ts
@@ -0,0 +1,57 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { FindProductRepositoryContract } from 'src/resources/product/contract/find-product.contract';
+import { UpdateProductRepositoryContract } from 'src/resources/product/contract/update-product.contract';
+import { Product } from 'src/resources/product/entity/product.entity';
+import { UpdateProductUseCase } from 'src/resources/product/usecase/update-product.usecase';
+
+@Injectable()
+export class UpdateProductService implements UpdateProductUseCase {
+  constructor(
+    private readonly updateProductRepository: UpdateProductRepositoryContract,
+    private readonly findProductRepository: FindProductRepositoryContract,
+  ) {}
+
+  async updateProduct(input: {
+    id: number;
+    name?: string;
+    image?: string;
+    description?: string;
+    price?: number;
+    categoryId?: number;
+    subcategoryId?: number;
+    discountPrice?: number;
+    active: boolean;
+  }): Promise<Product> {
+    const existingProduct = await this.findProductRepository.findProduct({
+      name: input.name,
+    });
+    const product = await this.findProductRepository.findProduct({
+      id: input.id,
+    });
+    if (!product) throw new NotFoundException('Product not found!');
+    if (existingProduct)
+      throw new ConflictException('Product name already exists!');
+    if (input.discountPrice && input.discountPrice >= input.price)
+      throw new ConflictException(
+        'Discount price must be less than the price!',
+      );
+    if (input.price < 1)
+      throw new ConflictException('Price must be greater than 1!');
+    const updatedProduct = await this.updateProductRepository.updateProduct({
+      id: input.id,
+      name: input.name,
+      image: input.image,
+      description: input.description,
+      price: input.price,
+      categoryId: input.categoryId,
+      subcategoryId: input.subcategoryId,
+      discountPrice: input.discountPrice,
+      active: input.active,
+    });
+    return updatedProduct;
+  }
+}
