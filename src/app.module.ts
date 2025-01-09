a6b7614f8f00ed4b3d3e366ab09e7bbf0d712a4b
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-09 00:49:12 -0300

    feat: add resource module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { InfraModule } from 'src/infra/infra.module';
+import { ResourceModule } from 'src/resources/resource.module';
 
 @Module({
-  imports: [InfraModule],
+  imports: [InfraModule, ResourceModule],
 })
 export class AppModule {}
