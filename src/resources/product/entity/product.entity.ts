d64719b3df4641b8d9a86f060269b2c62bac3568
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-09 00:00:14 -0300

    feat: add product barcode and sku

--- src/resources/product/entity/product.entity.ts
+++ src/resources/product/entity/product.entity.ts
@@ -3,35 +3,41 @@ import { SubCategory } from 'src/resources/subcategory/entity/subcategory.entity
 export class Product {
   id: number;
   name: string;
+  sku: string;
   description?: string;
   price: number;
   discountPrice?: number;
   subcategory?: SubCategory;
   active: boolean;
   image?: string;
+  barcode: string;
   createdAt: Date;
   updatedAt?: Date;
 
   constructor(
     id: number,
     name: string,
+    sku: string,
     description: string,
     price: number,
     discountPrice: number,
     subcategory: SubCategory,
     active: boolean,
     image: string,
+    barcode: string,
     createdAt: Date,
     updatedAt: Date,
   ) {
     this.id = id;
     this.name = name;
+    this.sku = sku;
     this.description = description;
     this.price = price;
     this.discountPrice = discountPrice;
     this.subcategory = subcategory;
     this.active = active;
     this.image = image;
+    this.barcode = barcode;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
