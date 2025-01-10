f2bf03150e5ff34ce4c29335340bfc51cd5258a7
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 00:18:58 -0300

    feat: add create product controller

---
+++ src/resources/product/controller/create-product.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateProductDto } from 'src/resources/product/dto/create-product.dto';
+import { Product } from 'src/resources/product/entity/product.entity';
+import { CreateProductUseCase } from 'src/resources/product/usecase/create-product.usecase';
+
+@ApiTags('Product')
+@Controller('product')
+export class CreateProductController {
+  constructor(private readonly useCase: CreateProductUseCase) {}
+
+  @Post()
+  async createProduct(@Body() input: CreateProductDto): Promise<Product> {
+    return this.useCase.createProduct({
+      name: input.name,
+      sku: input.sku,
+      barcode: input.barcode,
+      image: input.image,
+      description: input.description,
+      price: input.price,
+      subcategoryId: input.subcategoryId,
+      discountPrice: input.discountPrice,
+      active: input.active,
+    });
+  }
+}
