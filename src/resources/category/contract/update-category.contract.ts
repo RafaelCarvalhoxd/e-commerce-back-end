16c44fda5aabb3f1bcb57a94b8e0d836ee8fbda8
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 02:30:52 -0300

    fix: class name

--- src/resources/category/contract/update-category.contract.ts
+++ src/resources/category/contract/update-category.contract.ts
@@ -1,6 +1,6 @@
 import { Category } from 'src/resources/category/entity/category.entity';
 
-export abstract class UpdateCategoryRepositoryContracty {
+export abstract class UpdateCategoryRepositoryContract {
   abstract updateCategory(input: {
     id: number;
     name: string;
