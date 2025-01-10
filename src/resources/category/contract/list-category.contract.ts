01d2788d90535328314601ec32eb6e36d8147b23
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 02:30:39 -0300

    fix: make list filter optional

--- src/resources/category/contract/list-category.contract.ts
+++ src/resources/category/contract/list-category.contract.ts
@@ -1,5 +1,5 @@
 import { Category } from 'src/resources/category/entity/category.entity';
 
 export abstract class ListCategoryRepositoryContract {
-  abstract listCategory(input: { name: string }): Promise<Category[]>;
+  abstract listCategory(input: { name?: string }): Promise<Category[]>;
 }
