5df266b4927db5bf986d937a68b92d18ebc901a9
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 01:05:13 -0300

    feat: add category contracts

--- src/resources/category/contract/find-category.contract.ts
+++ src/resources/category/contract/find-category.contract.ts
@@ -3,6 +3,6 @@ import { Category } from 'src/resources/category/entity/category.entity';
 export abstract class FindCategoryContract {
   abstract findCategory(input: {
     id?: number;
-    name: string;
+    name?: string;
   }): Promise<Category>;
 }
