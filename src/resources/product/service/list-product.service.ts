52b1ecfb806dd7c110cc9c631696292689e48c51
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 00:22:19 -0300

    feat: add pagination

--- src/resources/product/service/list-product.service.ts
+++ src/resources/product/service/list-product.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Pagination } from 'src/common/pagination/pagination.common';
 import { ListProductRepositoryContract } from 'src/resources/product/contract/list-product.contract';
 import { Product } from 'src/resources/product/entity/product.entity';
 import { ListProductUseCase } from 'src/resources/product/usecase/list-product.usecase';
@@ -13,16 +14,25 @@ export class ListProductService implements ListProductUseCase {
     name?: string;
     minPrice?: number;
     maxPrice?: number;
-    categoryId?: number;
-    subcategoryId?: number;
-  }): Promise<Product[]> {
-    const products = await this.listProductRepository.listProduct({
+    categoryId?: number[];
+    subcategoryId?: number[];
+    page?: number;
+    limit?: number;
+    orderBy?: 'price' | 'name';
+    orderDirection?: 'ASC' | 'DESC';
+  }): Promise<Pagination<Product>> {
+    const [data, total] = await this.listProductRepository.listProduct({
       name: input.name,
       minPrice: input.minPrice,
       maxPrice: input.maxPrice,
       categoryId: input.categoryId,
       subcategoryId: input.subcategoryId,
+      page: input.page,
+      limit: input.limit,
+      orderBy: input.orderBy,
+      orderDirection: input.orderDirection,
     });
-    return products;
+
+    return Pagination.from(data, total, input.page, input.limit);
   }
 }
