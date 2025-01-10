c05fcc9ebcfca297484e083a5370a1dc895f5e9c
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 02:49:20 -0300

    fix: contract name

--- src/resources/category/service/update-category.service.ts
+++ src/resources/category/service/update-category.service.ts
@@ -4,7 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { FindCategoryRepositoryContract } from 'src/resources/category/contract/find-category.contract';
-import { UpdateCategoryRepositoryContracty } from 'src/resources/category/contract/update-category.contract';
+import { UpdateCategoryRepositoryContract } from 'src/resources/category/contract/update-category.contract';
 import { Category } from 'src/resources/category/entity/category.entity';
 import { UpdateCategoryUseCase } from 'src/resources/category/usecase/update-category.usecase';
 
@@ -12,7 +12,7 @@ import { UpdateCategoryUseCase } from 'src/resources/category/usecase/update-cat
 export class UpdateCategoryService implements UpdateCategoryUseCase {
   constructor(
     private readonly findCategoryRepository: FindCategoryRepositoryContract,
-    private readonly updateCategoryRepository: UpdateCategoryRepositoryContracty,
+    private readonly updateCategoryRepository: UpdateCategoryRepositoryContract,
   ) {}
 
   async updateCategory(input: { id: number; name: string }): Promise<Category> {
