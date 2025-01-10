d891a799db3e647e36d95ea157cccfdcd6c2d650
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 00:20:00 -0300

    feat: add pagination, order and change filters

--- src/resources/product/repository/list-product.repository.ts
+++ src/resources/product/repository/list-product.repository.ts
@@ -12,9 +12,13 @@ export class ListProductRepository implements ListProductRepositoryContract {
     name?: string;
     minPrice?: number;
     maxPrice?: number;
-    categoryId?: number;
-    subcategoryId?: number;
-  }): Promise<Product[]> {
+    categoryId?: number[];
+    subcategoryId?: number[];
+    page?: number;
+    limit?: number;
+    orderBy?: 'price' | 'name';
+    orderDirection?: 'ASC' | 'DESC';
+  }): Promise<[Product[], number]> {
     const products = this.ds
       .getRepository(ProductModel)
       .createQueryBuilder('p')
@@ -22,28 +26,49 @@ export class ListProductRepository implements ListProductRepositoryContract {
       .leftJoinAndSelect('s.category', 'c')
       .where('p.active = true');
 
-    if (input.name) {
-      products.andWhere('p.name LIKE :name', { name: `%${input.name}%` });
+    const filters = [
+      {
+        condition: input.name,
+        query: 'p.name LIKE :name',
+        params: { name: `%${input.name}%` },
+      },
+      {
+        condition: input.minPrice,
+        query: 'p.price >= :minPrice',
+        params: { minPrice: input.minPrice },
+      },
+      {
+        condition: input.maxPrice,
+        query: 'p.price <= :maxPrice',
+        params: { maxPrice: input.maxPrice },
+      },
+      {
+        condition: input.categoryId,
+        query: 'c.id IN (:...categoryId)',
+        params: { categoryId: input.categoryId },
+      },
+      {
+        condition: input.subcategoryId,
+        query: 's.id IN (:...subcategoryId)',
+        params: { subcategoryId: input.subcategoryId },
+      },
+    ];
+
+    filters.forEach(({ condition, query, params }) => {
+      if (condition) {
+        products.andWhere(query, params);
+      }
+    });
+
+    if (input.page ?? (1 && (input.limit ?? 10))) {
+      products.skip((input.page - 1) * input.limit).take(input.limit);
     }
 
-    if (input.minPrice) {
-      products.andWhere('p.price >= :minPrice', { minPrice: input.minPrice });
+    if (input.orderBy && input.orderDirection) {
+      products.orderBy(`p.${input.orderBy}`, input.orderDirection);
     }
 
-    if (input.maxPrice) {
-      products.andWhere('p.price <= :maxPrice', { maxPrice: input.maxPrice });
-    }
-
-    if (input.categoryId) {
-      products.andWhere('c.id = :categoryId', { categoryId: input.categoryId });
-    }
-
-    if (input.subcategoryId) {
-      products.andWhere('s.id = :subcategoryId', {
-        subcategoryId: input.subcategoryId,
-      });
-    }
-
-    return products.getMany();
+    const [data, total] = await products.getManyAndCount();
+    return [data, total];
   }
 }
