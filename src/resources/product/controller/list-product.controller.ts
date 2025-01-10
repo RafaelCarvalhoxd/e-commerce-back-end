317f80f6b9d082b0e27937fdb2af07f88f22c9fb
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-10 00:19:13 -0300

    feat: add list products controller

---
+++ src/resources/product/controller/list-product.controller.ts
@@ -0,0 +1,88 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ListProductUseCase } from 'src/resources/product/usecase/list-product.usecase';
+import { Pagination } from 'src/common/pagination/pagination.common';
+import { Product } from 'src/resources/product/entity/product.entity';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PaginationPagePipe } from 'src/common/pipes/pagination-page.pipe';
+import { PaginationLimitPipe } from 'src/common/pipes/pagination-limit.pipe';
+
+@ApiTags('Product')
+@Controller('product')
+export class ListProductController {
+  constructor(private readonly listProductUseCase: ListProductUseCase) {}
+
+  @Get()
+  @ApiQuery({
+    name: 'name',
+    required: false,
+    description: 'Filter by product name',
+  })
+  @ApiQuery({
+    name: 'minPrice',
+    required: false,
+    description: 'Filter by minimum price',
+  })
+  @ApiQuery({
+    name: 'maxPrice',
+    required: false,
+    description: 'Filter by maximum price',
+  })
+  @ApiQuery({
+    name: 'categoryId',
+    required: false,
+    description: 'Filter by category ID',
+  })
+  @ApiQuery({
+    name: 'subcategoryId',
+    required: false,
+    description: 'Filter by subcategory ID',
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'The page number for pagination',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'The number of products per page',
+  })
+  @ApiQuery({
+    name: 'orderBy',
+    required: false,
+    description: 'Field to order the products by (name or price)',
+  })
+  @ApiQuery({
+    name: 'orderDirection',
+    required: false,
+    description: 'Order direction (ASC or DESC)',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved paginated products',
+    type: Pagination,
+  })
+  async list(
+    @Query('name') name: string,
+    @Query('minPrice') minPrice: number,
+    @Query('maxPrice') maxPrice: number,
+    @Query('categoryId') categoryId: number[],
+    @Query('subcategoryId') subcategoryId: number[],
+    @Query('page', PaginationPagePipe) page: number,
+    @Query('limit', PaginationLimitPipe) limit: number,
+    @Query('orderBy') orderBy: 'price' | 'name',
+    @Query('orderDirection') orderDirection: 'ASC' | 'DESC',
+  ): Promise<Pagination<Product>> {
+    return this.listProductUseCase.listProduct({
+      name: name,
+      categoryId: categoryId,
+      limit: limit,
+      maxPrice: maxPrice,
+      minPrice: minPrice,
+      orderBy: orderBy,
+      orderDirection: orderDirection,
+      page: page,
+      subcategoryId: subcategoryId,
+    });
+  }
+}
