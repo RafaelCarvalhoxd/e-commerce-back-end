2fb41d93decfd08945b65f9ce1570d78f8ecb5ad
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-14 20:13:21 -0300

    feat: add staff module

--- src/resources/staff/staff.module.ts
+++ src/resources/staff/staff.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BcryptModule } from 'src/resources/bcrypt/bcrypt.module';
+import { RoleModule } from 'src/resources/roles/roles.module';
 import { StaffModel } from 'src/resources/staff/model/staff.model';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StaffModel]), BcryptModule],
+  imports: [TypeOrmModule.forFeature([StaffModel]), BcryptModule, RoleModule],
   providers: [],
   exports: [],
 })
