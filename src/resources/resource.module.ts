194880ba66670caf9df5bf1c64255737b7be9098
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-01-14 20:13:43 -0300

    feat: add bcrypt, role & staff modules

--- src/resources/resource.module.ts
+++ src/resources/resource.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { BcryptModule } from 'src/resources/bcrypt/bcrypt.module';
 import { CategoryModule } from 'src/resources/category/category.module';
 import { ProductModule } from 'src/resources/product/product.module';
+import { RoleModule } from 'src/resources/roles/roles.module';
+import { StaffModule } from 'src/resources/staff/staff.module';
 import { SubCategoryModule } from 'src/resources/subcategory/subcategory.module';
 
 @Module({
-  imports: [ProductModule, SubCategoryModule, CategoryModule],
-  exports: [ProductModule, SubCategoryModule, CategoryModule],
+  imports: [
+    ProductModule,
+    SubCategoryModule,
+    CategoryModule,
+    StaffModule,
+    BcryptModule,
+    RoleModule,
+  ],
+  exports: [
+    ProductModule,
+    SubCategoryModule,
+    CategoryModule,
+    StaffModule,
+    BcryptModule,
+    RoleModule,
+  ],
 })
 export class ResourceModule {}
