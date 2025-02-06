366f6c2cce92014bb9e0253a2ab8d0f096ade4f2
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-02-06 18:56:54 -0300

    fix: token invalid signature error

--- src/common/guard/auth.guard.ts
+++ src/common/guard/auth.guard.ts
@@ -19,13 +19,17 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokemFromHeader(request);
     if (!token) throw new UnauthorizedException('Token not found');
-    const payload = await this.jwt.verify(
-      token,
-      this.configService.get('jwt.secret'),
-    );
-    request['user'] = payload;
-    if (!payload) throw new UnauthorizedException('Invalid token');
-    return true;
+    try {
+      const payload = await this.jwt.verify(
+        token,
+        this.configService.get('jwt.secret'),
+      );
+      request['user'] = payload;
+      if (!payload) throw new UnauthorizedException('Invalid token');
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token', error.message);
+    }
   }
 
   private extractTokemFromHeader(request: Request): string {
