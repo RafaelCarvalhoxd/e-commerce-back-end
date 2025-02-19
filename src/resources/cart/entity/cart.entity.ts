df884e0c4a9cf54ea3d774366f306e860df66700
Faelzxd <devrafaelcarvalho14@gmail.com>
2025-02-19 01:56:35 -0300

    feat: add quantity to cart entity

--- src/resources/cart/entity/cart.entity.ts
+++ src/resources/cart/entity/cart.entity.ts
@@ -3,12 +3,20 @@ import { Product } from 'src/resources/product/entity/product.entity';
 export class Cart {
   id: number;
   product: Product;
+  quantity: number;
   createdAt: Date;
   updatedAt: Date;
 
-  constructor(id: number, product: Product, createdAt: Date, updatedAt: Date) {
+  constructor(
+    id: number,
+    product: Product,
+    createdAt: Date,
+    updatedAt: Date,
+    quantity: number,
+  ) {
     this.id = id;
     this.product = product;
+    this.quantity = quantity;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
